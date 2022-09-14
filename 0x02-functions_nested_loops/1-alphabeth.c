812664cc9c7f5b76b3e4f70f07fe23ae8d04dce1
Anita5810 <anitanonyeh1@gmail.com>
2022-09-14 02:18:31 -0700

    finding

---
+++ 0x02-functions_nested_loops/1-alphabeth.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabeth in lowercase
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabeth(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
