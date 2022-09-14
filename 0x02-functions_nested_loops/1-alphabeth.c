3a701f4892ae06e3144d30c2382d4087d8fe5c8d
Anita5810 <anitanonyeh1@gmail.com>
2022-09-14 03:00:01 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabeth.c
+++ 0x02-functions_nested_loops/1-alphabeth.c
@@ -4,13 +4,26 @@
  *
  *Return: 0
 */
-void print-alphabet(void)
+void print_alphabet(void)
 {
      char c;
 
      for (c = 'a'; c <= 'z'; c++)
+}^C
+root@a93756abbbca:/alx-low_level_programming/0x02-functions_nested_loops# cat > 1-alphabeth.c
+#include "main.h"
+/**
+ *print_alphabet - function to print abc
+ *
+ *Return: 0
+*/
+void print_alphabet(void)
 {
-     _putchar(c);
-}
+     char c;
+
+     for (c = 'a'; c <= 'z'; c++)
+     {
+          _putchar(c);
+     }
 _putchar('\n');
 }
