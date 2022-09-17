40219eda695aad340f9de546916d003ccc9c78ac
Anita5810 <anitanonyeh1@gmail.com>
2022-09-17 05:08:45 -0700

    commiting

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,12 @@
 
 /**
  * _isupper - check for upper case letter
- * @c : character to check the case
- * Return: 1 if it is, otherwise 0
+ * @c: character to check the case
+ * Return:0 or 1
  */
 
 int _isupper(int c)
 {
-	if ((c >= '65') && (c <=  '90'))
-	{
-	  return (1);
-	}
-	return (0);
+        return (c >= 65 && c <= 90);
 
 }
